64da7c32594fc6046db7791c57c784fcf58f72e8
Dexesttp <dexes.ttp@gmail.com>
2017-01-29 04:19:39 +0100

    Added config.json file handing.

--- src/js/config.js
+++ src/js/config.js
@@ -4,9 +4,8 @@ const config = {};
 
 exports.initConfig = function(extConfig) {
     extConfig = extConfig || {};
-    config.viewFolder = path.resolve(__dirname, "../../", extConfig.viewFolder || "src/views");
-    config.imageFolder = path.resolve(__dirname, "../../", extConfig.imageFolder || "images");
     config.database = path.resolve(__dirname, "../../", extConfig.database || "database");
+    config.imageFolder = path.resolve(__dirname, "../../", extConfig.imageFolder || "images");
     config.staticFolder = path.resolve(__dirname, "../../", extConfig.staticFolder || "public");
     config.port = extConfig.port || 3000;
 }
--- src/js/server.js
+++ src/js/server.js
@@ -1,16 +1,26 @@
-const express = require("express");
 const bodyParser = require("body-parser");
 const busboy = require("connect-busboy");
+const express = require("express");
+const fs = require('fs');
+const path = require("path");
+
 const confHelper = require("./config");
 const api = require("./api");
 const routes = require("./routes");
 const database = require("./database");
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
 app.use(busboy());
 
-confHelper.initConfig();
+try {
+    var configFilePath = path.resolve(__dirname, "../..", "config.json");
+    var configContents = fs.readFileSync(configFilePath, 'utf8');
+    console.log(`Found configuration file : ${configFilePath}`);
+    confHelper.initConfig(JSON.parse(configContents));
+} catch(e) {
+    confHelper.initConfig();
+}
 const config = confHelper.config;
 console.log(config);
 
